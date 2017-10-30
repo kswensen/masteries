131a96a0b9e5537ea5a269fa2832c273c2ebc4f3
StayinNasty <keganswensen@gmail.com>
2017-10-30 10:34:20 -0600

    Fixed Child.js

--- masteries/src/Components/ParentChildren/Child.js
+++ masteries/src/Components/ParentChildren/Child.js
@@ -1,48 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './parentChild.css';
 
-export default class Child extends Component {
-    constructor() {
-        super();
-
-        this.state = {
-            message: '',
-            savedMessages: []
-        }
+export default function Child(props) {
+    const sayMessage = (event) => {
+        alert(props.message);
     }
 
-    componentWillReceiveProps(nextProps) {
-        this.setState({
-            message: nextProps.message
-        })
-    }
-
-    saveMessage(message){
-        this.setState({
-            savedMessages: [...this.state.savedMessages, message]
-        });
-    }
-
-    render() {
-        let mappedMessages = this.state.savedMessages.map((message, i) => {
-            return (
-                <div key={i}>
-                    <p>{message}</p>
-                </div>
-            )
-        });
-        return (
-            <div className='child'>
-                <div>
-                    <h1>This is the child</h1>
-                    <h1>Message from parent: {this.state.message}</h1>
-                    <button onClick={() => {this.saveMessage(this.state.message)}}>Save Message</button>
-                    <button onClick={() => { this.props.removeMessage() }}>Clear Message</button>
-                </div>
-                <div>
-                    {mappedMessages}
-                </div>
-            </div>
-        )
-    }
+    return (
+        <div className='child'>
+            <h1>{props.message}</h1>
+            <button onClick={sayMessage}>Alert</button>
+            <button onClick={() => {props.removeMessage()}}>Clear</button>
+        </div>
+    )
 }
